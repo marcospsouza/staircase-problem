84cabd75ee65c45ec5288543006a6fe570172b06
marcos <marcos_souza96@hotmail.com>
2021-04-18 20:12:05 -0300

    slightly different algorithm in python

---
+++ staircase.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        for j in range(n):
+            if i+j >= n-1:
+                print('#', end = '')
+            else:
+                print(' ', end = '')
+        print('')
+        
+if __name__ == '__main__':
+    
+    n = int(input().strip())
+    staircase(n)
